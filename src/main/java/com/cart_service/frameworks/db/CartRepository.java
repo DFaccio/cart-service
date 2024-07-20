837c1e7f0a7721b0fd9d8cdb8fab1ba9c68b7661
Danilo Faccio <facciodrums@gmail.com>
2024-07-20 19:04:27 -0300

    ajuste swagger e repository

--- src/main/java/com/cart_service/frameworks/db/CartRepository.java
+++ src/main/java/com/cart_service/frameworks/db/CartRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartRepository extends ReactiveMongoRepository<Cart, String> {
 
     @Query(value="{$and: [{'costumerId':{$eq:?0}},{'cartStatus':{$eq:?1}}]}")
--- src/main/java/com/cart_service/util/config/documentation/SwaggerConfig.java
+++ src/main/java/com/cart_service/util/config/documentation/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.cart_service.util.config.documentation;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
